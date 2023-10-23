7de2c551af535b65da2e8db64752ec1cc007fd28
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-10-23 09:12:27 +0000

    z

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,46 +1,47 @@
 import axios from 'axios';
-import React,{useState,useEffect} from 'react';
-let citySearch = '';
-function Weather(){
-    
-    let [city,setCity] = useState('');
-    let [data,setData] = useState([]);
-    let [temp,setTemp] = useState(null);
-    let [des,setDes] = useState('');
-    let [val,setVal] = useState(city);
+import React, { useState, useEffect } from 'react';
 
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&appid=85d6588eaee5a0d134b1e5e179482061&units=metric`
+function Weather() {
+  const [city, setCity] = useState('');
+  const [data, setData] = useState([]);
+  const [temp, setTemp] = useState(null);
+  const [des, setDes] = useState('');
 
-    function handleSearch(){
-        axios.get(endpoint)
-        .then((data)=>{
-           
-            setData(data.data)
-            setTemp(data.data.main.temp)
-            setDes(data.data.weather[0].description)
-            citySearch = city;
-            setCity('')
-            console.log(data)
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&appid=85d6588eaee5a0d134b1e5e179482061&units=metric`;
+
+  useEffect(() => {
+    if (city.trim() !== '') {
+      axios
+        .get(endpoint)
+        .then((response) => {
+          setData(response.data);
+          setTemp(response.data.main.temp);
+          setDes(response.data.weather[0].description);
+          setCity(''); // Clear the input field
+          console.log(response);
         })
-        .catch((e)=>{
-            console.log(e)
-        })
+        .catch((e) => {
+          console.log(e);
+        });
     }
-    // useEffect(()=>{
-    //     handleSearch()
-    // },[city])
+  }, [city]);
 
-    return (
-        <div>
-            <input className='search' value={city}  type='text' placeholder='Enter city' onChange={(e)=> setCity(e.target.value)}></input>
-            <button onClick={handleSearch}>Search</button>
-            <div className='weather'>
-               <h1>{citySearch}</h1>
-               <h2>{temp} F</h2>
-               <h3>{des}</h3>
-                
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        className="search"
+        value={city}
+        type="text"
+        placeholder="Enter city"
+        onChange={(e) => setCity(e.target.value)}
+      ></input>
+      <div className="weather">
+        <h1>{city}</h1>
+        <h2>{temp} F</h2>
+        <h3>{des}</h3>
+      </div>
+    </div>
+  );
 }
-export default Weather
+
+export default Weather;
